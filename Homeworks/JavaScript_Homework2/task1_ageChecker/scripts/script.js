d89fabd8d6b603610d5c2102a29b47b8493eceef
AdrianaTomovska <61731660+AdrianaTomovska@users.noreply.github.com>
2026-01-13 11:14:26 +0100

    edited

--- Homeworks/JavaScript_Homework2/task1_ageChecker/scripts/script.js
+++ Homeworks/JavaScript_Homework2/task1_ageChecker/scripts/script.js
@@ -1,12 +1,22 @@
 let input = prompt('Enter your age:');
-let age = parseInt(input);
 
-if (!input || isNaN(age) || age < 0) {
-    alert('Please enter a valid age.');
-} else if (age <= 12) {
-    console.log('You are ' + age + '.' + ' ' + 'You are a child.');
-} else if (age >= 13 && age <= 17) {
-    console.log('You are ' + age + '.' + ' ' + 'You are a teenager.');
-} else if (age >= 18) {
-    console.log('You are ' + age + '.' + ' ' + 'You are an adult.');
+if (input === null || input === '') {
+    console.log('Input is empty or cancelled. Please enter a valid age.');
+} else {
+    let age = parseInt(input);
+
+    if (isNaN(age) || age < 0) {
+        console.log('Please enter a valid age.');
+    }
+
+    else {
+        if (age <= 12) {
+            console.log('You are ' + age + '.' + ' ' + 'You are a child.');
+        } else if (age >= 13 && age <= 17) {
+            console.log('You are ' + age + '.' + ' ' + 'You are a teenager.');
+        } else if (age >= 18) {
+            console.log('You are ' + age + '.' + ' ' + 'You are an adult.');
+        }
+
+    }
 }
